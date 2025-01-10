7378842e468b170e0130ba8e3625975dcb3e6cb7
Vitor Comin <vitorgabrielcomin@gmail.com>
2025-01-10 17:44:45 -0300

    Adjusting antd version, tests, adjustments

--- src/services/OpenWeatherApi.tsx
+++ src/services/OpenWeatherApi.tsx
@@ -1,6 +1,6 @@
-export const getCities = async (value: string) => {
-  const axios = require("axios");
+import axios from "axios";
 
+export const getCities = async (value: string) => {
   return axios.get(
     `https://api.openweathermap.org/data/2.5/find?q=${value}&units=metric&appid=b6432528466c2ddd5717e9a9ae56b0f9&lang=en`
   );
